9077c8382f2f91bf66cc227eaacd5b344b414b3a
Christian Cmehil-Warn <github@christiancw.anonaddy.com>
2023-09-03 14:02:45 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,48 +1,69 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 # Streamlit app title
-st.title('Zip Code Information App')
+st.title('Congress Voting Record Dashboard')
+
+#Loads Voting Google sheet
+@st.cache_data(ttl=600)
+def load_data():
+    house_csv_url = "https://docs.google.com/spreadsheets/d/1gPBdBrqVCbtuy7f1bjOdCDUzEv5RqbbU1yYAr3KoHYE/export?format=csv&gid=1289123714"
+    senate_csv_url = "https://docs.google.com/spreadsheets/d/1gPBdBrqVCbtuy7f1bjOdCDUzEv5RqbbU1yYAr3KoHYE/export?format=csv&gid=0"
+    return (pd.read_csv(house_csv_url), pd.read_csv(senate_csv_url))
+
+house_data, senate_data = load_data()
+st.write("Original Data provided [here](https://docs.google.com/spreadsheets/d/1gPBdBrqVCbtuy7f1bjOdCDUzEv5RqbbU1yYAr3KoHYE/edit#gid=1289123714)")
+with st.expander("Raw Data"):
+    st.write(house_data)
+    st.write(senate_data)
 
 # Input for zip code
 zip_code = st.text_input('Enter your zip code:')
 
-# Function to fetch state information
-def get_state_info(zip_code):
-    state_api_url = f'https://openstates.org/api/v1/legislators/geo/?lat=0&long=0&apikey=YOUR_OPEN_STATES_API_KEY'
-    response = requests.get(state_api_url)
-    data = response.json()
-    if data:
-        return data[0]['state']
-    return None
+# Function to fetch Congress people for ZIP
+def get_rep_by_district(zip_code):
+    # Replace [ZIP] with your actual ZIP code
+    url = f"https://whoismyrepresentative.com/getall_mems.php?zip={zip_code}&output=json"
+    response = requests.get(url)
 
-# Function to fetch House of Representatives district information
-def get_rep_district(zip_code):
-    congress_api_url = f'https://theunitedstates.io/congress-phrases/congresses/117/sessions/1/congresspersons/house/?format=json'
-    response = requests.get(congress_api_url)
-    data = response.json()
-    for congressperson in data:
-        if zip_code in congressperson['district']['postal_codes']:
-            return congressperson['name'], congressperson['district']['name']
-    return None, None
+    if response.status_code == 200:
+        data = response.json()
+        members = data['results']
+        return members
+    else:
+        return False
+        
 
 # Check if the user has entered a valid zip code
 if zip_code and zip_code.isdigit() and len(zip_code) == 5:
-    state_info = get_state_info(zip_code)
-    rep_name, rep_district = get_rep_district(zip_code)
-
-    if state_info:
-        st.write(f'**State:** {state_info}')
-
-    if rep_name and rep_district:
-        st.write(f'**House of Representatives District:** {rep_district}')
-        st.write(f'**Your Representative:** {rep_name}')
-    elif rep_name:
-        st.write(f'**Your Representative:** {rep_name}')
+    members = get_rep_by_district(zip_code)
+    if not members:
+        st.error('API Failed', icon="🚨")
     else:
-        st.warning('House of Representatives information not found for this zip code.')
+        house_local = [reps for reps in members if reps.get("district") != ""]
+        senate_local = [reps for reps in members if reps.get("district") == ""]  
+    
+        st.write("## House Reps")
+        if len(house_local) > 1:
+            st.info("More than one Representative for Zip")
+        # st.write(house_local)
+        district_lists = []
+        for rep in house_local:
+            state = rep['state']
+            district = rep['district']
+            district = district.zfill(2)
+            district_lists.append(state + '-' + district)
+        st.write(house_data.loc[house_data['CD'].isin(district_lists)])
+
+
+        st.write("## Senators")
+        state = senate_local[0]['state']
+        st.write(state)
+        states = {"AL":"Alabama","AK":"Alaska","AZ":"Arizona","AR":"Arkansas","CA":"California","CO":"Colorado","CT":"Connecticut","DE":"Delaware","FL":"Florida","GA":"Georgia","HI":"Hawaii","ID":"Idaho","IL":"Illinois","IN":"Indiana","IA":"Iowa","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","ME":"Maine","MD":"Maryland","MA":"Massachusetts","MI":"Michigan","MN":"Minnesota","MS":"Mississippi","MO":"Missouri","MT":"Montana","NE":"Nebraska","NV":"Nevada","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NY":"New York","NC":"North Carolina","ND":"North Dakota","OH":"Ohio","OK":"Oklahoma","OR":"Oregon","PA":"Pennsylvania","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington","WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming"}
+        st.write(states[state])
+        st.write(senate_data.loc[senate_data['State'] == states[state]])
 
 elif zip_code:
     st.warning('Please enter a valid 5-digit zip code.')
 
-# Note: Replace 'YOUR_OPEN_STATES_API_KEY' with your actual Open States API key.
